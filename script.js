5f8bae2d58eab169da1730ea943473fdca93ec1a
AlexAlpine <alex6122223@gmail.com>
2024-08-30 13:28:19 +0200

    second

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ async function getMovieRecommendations() {
         const poster = document.createElement('img');
         poster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
         poster.alt = movie.title;
+        const title = document.createElement('h3');
+        title.textContent = movie.title;
+
+        const rating = document.createElement('p');
+      rating.textContent = `Рейтинг: ${movie.vote_average}`;
+
+      movieElement.appendChild(poster);
+      movieElement.appendChild(title);
+      movieElement.appendChild(rating);
+      movieRecommendationsContainer.appendChild(movieElement);
+    });
+  } catch (error) {
+    console.error('Ошибка:', error);
+  }
+}
